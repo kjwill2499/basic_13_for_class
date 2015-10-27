b9cc8d49a4ad3596a7adfba00cd100cd0dd36604
Mike Hannon(code) <mjhannon@gmail.com>
2015-10-27 11:17:40 -0700

    create basic assignment

---
+++ basic13.js
@@ -0,0 +1,15 @@
+//Fork this repository and write these algorithms! Share with your colleagues!
+//1. print1To255: Print all integers from 1 to 255.
+//2. printOdds1To255: Print all the odd integers from 1 to 255.
+//3. printIntsAndSum1To255: Print integers from 0 to 255 and the running sum.
+// FOR ARRAYS make the array and pass it as an argument to this function!
+//4. printArrayVals: Print all the value in a given array --
+//5. printMaxOfArray: Print the largest element of an array.
+//6. printAverageOfArray: Print the average of all the elements value of an array.
+//7. returnOddsArray1to255: Return an aray with just the odd integers from 1-255.
+//8. squareArrayVals: Given an array, square each value in the array.
+//9.  returnCountOfElementsWithValueGreaterThanY: pass to arguments to the function, the array and y.  Return the number of elements in the array with a value greater than y.
+//10.  setNegativeValuesOfArrayToZero: Set all of the negative values in an array to zero and return the array.
+//11. printMaxMinAverageArrayVals:  given an array, print the min, max and average values of that array.
+//12. shiftArrayValsLeft:  Given an array, move all of the values of the array to the left, removing element 0, and having the new last element of the array be zero.
+//13. swapStringForNegativeValues:  Pass an array and a string into the function.  Replace all of the negative values with that string and return the array.  
